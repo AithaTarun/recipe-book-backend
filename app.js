b4f917735aa1759e7b4b1243805d47942eec2bfb
Aitha Tarun <aitha.tarun@gmail.com>
2021-02-10 22:42:59 +0530

    Fourth commit

--- app.js
+++ app.js
@@ -1,64 +1,12 @@
-const express = require('express');
+const app = require('./server');
 
-const app = express();
+const port = process.env.PORT || 3000;
 
-const mongoose = require('mongoose');
-
-mongoose.connect
+app.listen
 (
-  process.env.MONGO_DB_URL,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex : true
-  }
-).then
-(
+  port,
   ()=>
   {
-    console.log("Connected to database")
-  }
-).catch
-(
-  ()=>
-  {
-    console.log("Connection to database failed");
+    console.log("Server is running on port",port);
   }
 );
-
-app.use
-(
-  express.json()
-);
-
-app.use //To handle CORS error.
-  (
-    (request,response,next)=>
-    {
-      response.setHeader
-      ('Access-Control-Allow-Origin','*');
-
-      response.setHeader
-      (
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      );
-
-      response.setHeader
-      (
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS, PUT'
-      );
-
-      next();
-    }
-  );
-
-const userRoutes = require('./routes/user');
-app.use("/api/user",userRoutes);
-
-const recipeRoutes = require('./routes/recipe');
-app.use("/api/recipe",recipeRoutes);
-
-module.exports = app;
-
--- server.js
+++ server.js
@@ -1,12 +1,64 @@
-const app = require('./app');
+const express = require('express');
 
-const port = process.env.PORT || 3000;
+const app = express();
 
-app.listen
+const mongoose = require('mongoose');
+
+mongoose.connect
 (
-  port,
+  process.env.MONGO_DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+  }
+).then
+(
   ()=>
   {
-    console.log("Server is running on port",port);
+    console.log("Connected to database")
+  }
+).catch
+(
+  ()=>
+  {
+    console.log("Connection to database failed");
   }
 );
+
+app.use
+(
+  express.json()
+);
+
+app.use //To handle CORS error.
+  (
+    (request,response,next)=>
+    {
+      response.setHeader
+      ('Access-Control-Allow-Origin','*');
+
+      response.setHeader
+      (
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      );
+
+      response.setHeader
+      (
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS, PUT'
+      );
+
+      next();
+    }
+  );
+
+const userRoutes = require('./routes/user');
+app.use("/api/user",userRoutes);
+
+const recipeRoutes = require('./routes/recipe');
+app.use("/api/recipe",recipeRoutes);
+
+module.exports = app;
+
